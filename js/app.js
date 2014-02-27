9c5a9abc0e012e49a84022aec791e95f0ae8a434
Javi Manzano <javi.manzano.oller@gmail.com>
2014-02-27 19:28:50 +0000

    Finish level 4 - models, fixtures and related models

--- js/app.js
+++ js/app.js
@@ -1,42 +1,79 @@
 App = Ember.Application.create();
 
-App.PRODUCTS = [
+// App.ApplicationAdapter = DS.RESTAdapter.extend();
+App.ApplicationAdapter = DS.FixtureAdapter.extend();
+
+App.Product = DS.Model.extend({
+	title: DS.attr('string'),
+	price: DS.attr('number'),
+	description: DS.attr('string'),
+	isOnSale: DS.attr('boolean'),
+	image: DS.attr('string'),
+	reviews: DS.hasMany('review', {async: true})
+});
+
+App.Review = DS.Model.extend({
+	text: DS.attr('string'),
+	reviewedAt: DS.attr('date')
+});
+
+App.Product.FIXTURES = [
 	{
+		id: 1,
 		title: 'Flint',
 		price: 20,
-		isOnSale: true,
 		description: 'But you seem to be kicking my seat. Pardon me, sir. Would you mind? Sure thing, pal. Sir? Who threw that? Sir, I challenge you to fisticuffs. Oh, I\'m boned.',
-		image: 'images/flint.png'
+		isOnSale: true,
+		image: 'images/flint.png',
+		reviews: [100, 101]
 	}, {
+
+		id: 2,
 		title: 'Birch',
 		price: 120,
 		isOnSale: true,
 		description: 'But you seem to be kicking my seat. Pardon me, sir. Would you mind? Sure thing, pal. Sir? Who threw that? Sir, I challenge you to fisticuffs. Oh, I\'m boned.',
 		image: 'images/birch.png'
 	}, {
+		id: 3,
 		title: 'Bow Drill',
 		price: 140,
-		isOnSale: true,
 		description: 'But you seem to be kicking my seat. Pardon me, sir. Would you mind? Sure thing, pal. Sir? Who threw that? Sir, I challenge you to fisticuffs. Oh, I\'m boned.',
+		isOnSale: true,
 		image: 'images/bow-drill.png'
 	}, {
+		id: 4,
 		title: 'Kindling',
 		price: 10,
-		isOnSale: true,
 		description: 'But you seem to be kicking my seat. Pardon me, sir. Would you mind? Sure thing, pal. Sir? Who threw that? Sir, I challenge you to fisticuffs. Oh, I\'m boned.',
+		isOnSale: true,
 		image: 'images/kindling.png'
 	}, {
+		id: 5,
 		title: 'Matches',
 		price: 30,
-		isOnSale: true,
 		description: 'But you seem to be kicking my seat. Pardon me, sir. Would you mind? Sure thing, pal. Sir? Who threw that? Sir, I challenge you to fisticuffs. Oh, I\'m boned.',
+		isOnSale: true,
 		image: 'images/matches.png'
 	}, {
+		id: 6,
 		title: 'Tinder',
 		price: 50,
-		isOnSale: true,
 		description: 'But you seem to be kicking my seat. Pardon me, sir. Would you mind? Sure thing, pal. Sir? Who threw that? Sir, I challenge you to fisticuffs. Oh, I\'m boned.',
+		isOnSale: true,
 		image: 'images/tinder.png'
+	}
+];
+
+App.Review.FIXTURES = [
+	{
+		id: 100,
+		product: 1,
+		text: 'Started a fire in no time!'
+	}, {
+		id: 101,
+		product: 1,
+		text: 'Not the brightest flame, but warm!'
 	}
 ];
 
@@ -44,7 +81,7 @@ App.Router.map(function() {
 	this.route('about');
 	this.route('credits');
 	this.resource('products', function() {
-		this.resource('product', {path: '/:title'});
+		this.resource('product', {path: '/:product_id'});
 	});
 	
 });
@@ -58,12 +95,6 @@ App.IndexController = Ember.Controller.extend({
 App.ProductsRoute = Ember.Route.extend({
 	model: function(params) {
 		console.log(params);
-		return App.PRODUCTS;
-	}
-});
-
-App.ProductRoute = Ember.Route.extend({
-	model: function(params) {
-		return App.PRODUCTS.findBy('title', params.title);
+		return this.store.findAll('Product');
 	}
 });
